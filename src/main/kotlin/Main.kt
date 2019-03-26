9538a96328e7a82f1c07519dd9253113b78c9550
Shota Iuchi <shotaiuchi.develop@gmail.com>
2019-03-26 21:40:48 +0900

    20190326
    kawara
    iuchi
    john
    yamazaki

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,29 @@
+import java.io.File
+import java.nio.file.Paths
+
 fun getText(): String = "Hello Kotlin"
 
 fun main(args: Array<String>) {
-    println(getText())
+    var f = Paths.get(args[0]).toFile()
+    if (f.exists()) {
+        f.forEachLine { wordWrap(it, args[1].toInt()).forEach { i -> println("${i.length} $i") } }
+    } else {
+        throw IllegalArgumentException("しんじゃったよ！")
+    }
+}
+
+fun wordWrap(mojiretsu:String , wrapmojisu:Int): List<String> {
+    val split = mojiretsu.split(" ")
+    var counter = 0
+
+    val result: MutableList<String> = MutableList(1) { "" }
+    split.forEach {word ->
+        if (counter + word.length >= wrapmojisu) {
+            result.add("")
+            counter = 0
+        }
+        result[result.lastIndex] += "$word "
+        counter = result[result.lastIndex].length
+    }
+    return result.toList()
 }
